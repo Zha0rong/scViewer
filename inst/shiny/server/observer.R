20e6931dde2acc9957a17753b7a347d5d3467bd0
Zha0rong Li <34935078+Zha0rong@users.noreply.github.com>
2023-05-08 13:27:28 -0400

    Add a progress bar

--- inst/shiny/server/observer.R
+++ inst/shiny/server/observer.R
@@ -8,21 +8,29 @@ observeEvent( input$Seurat_Object, {
 
 
 observeEvent( input$submit, {
+
+
+
+
   if (!is.null(reactivevalue$object_location)){
+    withProgress(message = 'Load Data Object',value = 0, {
+
+    n=8
     reactivevalue$SeuratObject=readRDS(reactivevalue$object_location)
-    print('Start Loading')
+    incProgress(1/n,detail = 'Start Loading')
 
     if (input$objecttype=='SingleCellExperiment') {
       reactivevalue$SeuratObject=as.Seurat(reactivevalue$SeuratObject)
     }
-    print('Finish Loading')
+    incProgress(1/n,detail = 'Finish Loading')
+
     reactivevalue$SeuratObject$ShinyGroup='SCViewer'
     reactivevalue$Experiment_Metadata=reactivevalue$SeuratObject@meta.data
     DefaultAssay(reactivevalue$SeuratObject)='RNA'
     output$MainFigure=renderPlot(DimPlot(reactivevalue$SeuratObject))
     updateSelectizeInput(session = session,inputId = 'reduction',choices =names((reactivevalue$SeuratObject@reductions)),selected = NULL)
     updateSelectizeInput(session = session,inputId = 'GenesToInterrogate',choices =rownames(reactivevalue$SeuratObject),selected = NULL)
-    print('Finish Loading Genes')
+    incProgress(1/n,detail = 'Finish parsing Genes')
 
     updateSelectizeInput(session = session,inputId = 'variabletogroup',choices=colnames(reactivevalue$Experiment_Metadata)
                          [
@@ -31,22 +39,23 @@ observeEvent( input$submit, {
     ]
     ,selected = 'ShinyGroup')
 
-    print('Finish Loading Variables to Group')
+    incProgress(1/n,detail = 'Finish Parsing Columns step 1')
+
     updateSelectizeInput(session = session,inputId = 'variabletosplit',choices=colnames(reactivevalue$Experiment_Metadata)
                          [
       !grepl("nCount",colnames(reactivevalue$Experiment_Metadata))&!grepl("nFeature",colnames(reactivevalue$Experiment_Metadata))&!grepl("^percent.",colnames(reactivevalue$Experiment_Metadata))
     #  &!grepl("_res.",colnames(reactivevalue$Experiment_Metadata))
     ]
     ,selected = 'ShinyGroup')
-    print('Finish Loading Variables to Split')
+    incProgress(1/n,detail = 'Finish Parsing Columns step 2')
 
     updateSelectizeInput(session = session,inputId = 'SampleColumn',choices=colnames(reactivevalue$Experiment_Metadata)
                          [
       !grepl("nCount",colnames(reactivevalue$Experiment_Metadata))&!grepl("nFeature",colnames(reactivevalue$Experiment_Metadata))&!grepl("^percent.",colnames(reactivevalue$Experiment_Metadata))
     #  &!grepl("_res.",colnames(reactivevalue$Experiment_Metadata))
     ]
-    ,selected = 'ShinyGroup')
-    print('Finish Loading Sample Column')
+    ,selected = NULL)
+    incProgress(1/n,detail = 'Finish Parsing Columns step 3')
 
     updateSelectizeInput(session = session,inputId = 'PlotGroup',choices=colnames(reactivevalue$Experiment_Metadata)
                          [
@@ -55,7 +64,7 @@ observeEvent( input$submit, {
     ]
     ,selected = NULL)
 
-    print('Finish Loading Plot Group')
+    incProgress(1/n,detail = 'Finish Parsing Columns step 4')
 
     updateSelectizeInput(session = session,inputId = 'BarGraph1',choices=colnames(reactivevalue$Experiment_Metadata)
                          [
@@ -81,10 +90,17 @@ observeEvent( input$submit, {
     updateSelectizeInput(session = session,inputId = 'Assay',choices=names(reactivevalue$SeuratObject@assays),selected = NULL)
     updateSelectizeInput(session = session,inputId = 'DGEGroup1',choices=DGE_Group_Candidate,selected = NULL)
     updateSelectizeInput(session = session,inputId = 'DGEGroup2',choices=DGE_Group_Candidate,selected = NULL)
-    print('Finish Loading DGE Group')
+    incProgress(1/n,detail = 'Finish Parsing Columns step 5')
+
+
+
+    })
 
 }
-})
+}
+
+
+)
 
 
 DGEListener <- reactive({
@@ -104,7 +120,6 @@ BarGraphListener <- reactive({
 observeEvent( input$SampleColumn, {
   if (!is.null(input$SampleColumn)&!is.null(reactivevalue$SeuratObject)){
   updateSelectInput(session = session,inputId = 'SampletoSubset',choices=unique(reactivevalue$Experiment_Metadata[,input$SampleColumn]),selected = NULL)
-  print("Finished loading SampletoSubset")
 }
 })
 
