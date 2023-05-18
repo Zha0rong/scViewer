54751d3d2508bb66ad99c451df6213eac31c2fe4
Zha0rong Li <34935078+Zha0rong@users.noreply.github.com>
2023-05-18 09:57:26 -0400

    Add in NAMESPACE dependency

--- R/runApp.R
+++ R/runApp.R
@@ -1,13 +1,15 @@
 #' Run scViewer shiny app
-#'
+#' @import shiny
+#' @import shinyjs
+#' @import shinythemes
+#' @import data.table
+#' @import Seurat
+#' @import SingleCellExperiment
+#' @import ggplot2
 #' @return The shiny app will open
 #'
-#' @param dev Run the applicaiton in developer mode
+#' @param filelocation The directory of sce object or seurat object. If not specific the file can be uploaded inside the app.
 #'
-#' @examples
-#' \dontrun{
-#' scViewer()
-#' }
 #' @export
 
 scViewer <- function(filelocation=NULL) {
